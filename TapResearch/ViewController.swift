25637e7af57e67ab17897c40de716a741af9a737
Taylor Smith <taylor@taylorsmithcreates.com>
2021-07-29 08:47:15 -0600

    Initial Commit

---
+++ TapResearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapResearch
+//
+//  Created by Taylor Smith on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
